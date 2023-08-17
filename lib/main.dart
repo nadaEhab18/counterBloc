4a7a6db5c95408897136ee085b24391626c7185c
nadaEhab18 <nehab7947@gmail.com>
2023-08-17 13:02:51 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.purple,
         ),
         home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
